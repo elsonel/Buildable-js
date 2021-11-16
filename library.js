30eb56e4f167a825f88d4324480cbc37c91f1420
Elson <elsonliang@gmail.com>
2021-11-16 03:58:48 -0500

    Create Module class

--- library.js
+++ library.js
@@ -26,18 +26,35 @@ const Library = function (id) {
     startX = e.clientX;
     startY = e.clientY;
 
-    setActiveModule(allModules.find((module) => module.id == e.target.id));
+    const clickedModule = getModuleByID(getOuterModule(e.target).id);
+    if (clickedModule) {
+      setActiveModule(clickedModule);
+      clickedModule.setCursor(e.offsetX, e.offsetY, e.target.clientWidth, e.target.clientHeight)
+    }
+
+    render();
   }
 
   function onMove (e) {
-    if (!isMouseDown || !activeModule) return;
+    if (!isMouseDown && !activeModule) {
+      const module = getModuleByID(getOuterModule(e.target).id);
+      if (module) module.setCursor( e.offsetX, 
+                                    e.offsetY, 
+                                    e.target.clientWidth, 
+                                    e.target.clientHeight)
+    }
 
-    const deltaX = (e.clientX - startX);
-    const deltaY = (e.clientY - startY);
-    addPos(deltaX, deltaY, activeModule);
+    if (isMouseDown && activeModule) {
+      const deltaX = (e.clientX - startX);
+      const deltaY = (e.clientY - startY);
+      startX = e.clientX;
+      startY = e.clientY;
 
-    startX = e.clientX;
-    startY = e.clientY;
+      if (activeModule.cursor == 'move') activeModule.addPos(deltaX, deltaY);
+      else activeModule.addSize(deltaX, deltaY);
+    }
+
+    render();
   }
 
   function onRelease(e) {
@@ -47,6 +64,16 @@ const Library = function (id) {
 
 
    /* Private Functions */
+
+  function getOuterModule(target) {
+    if (target.parentElement.id == canvasElement.id || target.id == canvasElement.id) return target;
+    else return getOuterModule(target.parentElement);
+  }
+
+  function getModuleByID(id) {
+    /*https://javascript.info/bubbling-and-capturing*/
+    return allModules.find((module) => module.id == id);
+  }
 
   function setActiveModule(module) {
     activeModule = module;
@@ -59,18 +86,10 @@ const Library = function (id) {
     render();
   }
 
-  function addPos(xPos, yPos, module) {
-    module.xPos += xPos;
-    module.yPos += yPos;
-
-    render();
-  }
-
   function render() {
     [...allModules].reverse().forEach(module => {
 
       let moduleElement = null;
-
       const array = canvasElement.children;
       for(let i = 0; i < array.length; i++)
         if (array[i].id === module.id) {
@@ -82,38 +101,144 @@ const Library = function (id) {
         moduleElement = document.createElement('div');
         moduleElement.className = "module";
         moduleElement.id = module.id;
-        moduleElement.innerHTML = module.id;
+        moduleElement.style.position = 'absolute';
+        //moduleElement.style.paddingTop = '10px';
+        //moduleElement.innerHTML = module.id;
+
+        /* snapping test */
+
+        const parent = moduleElement;
+
+        child1 = document.createElement('div');
+        child1.style.width = '100%';
+        child1.style.height = `${module.height * 0.5}px`;
+        child1.style.backgroundColor = 'red';
+
+        parent.appendChild(child1);
       }
+
+      moduleElement.style.cursor = module.cursor;
+      //canvasElement.style.cursor = activeModule ? module.cursor : "default";
 
       moduleElement.style.top = module.yPos;
       moduleElement.style.left = module.xPos;
-      moduleElement.style.width = module.width;
-      moduleElement.style.height = module.height;
+      moduleElement.style.width = `${module.width}px`;
+      moduleElement.style.height = `${module.height}px`
 
       canvasElement.appendChild(moduleElement)
     });
   }
 
-
    /* Public Functions */
 
-  this.createModule = function () {
-    const module = {};
-    module.id = uuidv4();
-
-    module.xPos = 0;
-    module.yPos = 0;
-    module.width = "20%";
-    module.height = "20%";
-
-    allModules.push(module); 
+  this.createModule = function () {    
+    allModules.push(new Module()); 
 
     render();
   }
 
 }
 
-const l = new library("canvas");
-l.createModule();
+const Module = function() {
+
+  this.id = uuidv4();
+  this.cursor = null;
+
+  this.xPos = 400;
+  this.yPos = 400;
+  this.width = 200;
+  this.height = 200;
+
+  this.canMoveX = true;
+  this.canMoveY = true;
+  
+  this.canSize_N = true;
+  this.canSize_S = true;
+  this.canSize_W = true;
+  this.canSize_E = true;
+
+  this.addPos = function(xPos, yPos) {
+    if (this.canMoveX) this.xPos += xPos;
+    if (this.canMoveY) this.yPos += yPos;
+  }
+
+  this.addSize = function(xPos, yPos) {
+    switch(this.cursor) {
+      case 'nw-resize':
+        this.addSizeDirection("n-resize", yPos);
+        this.addSizeDirection("w-resize", xPos);
+        break;
+      case 'ne-resize':
+        this.addSizeDirection("n-resize", yPos);
+        this.addSizeDirection("e-resize", xPos);
+        break;
+      case 'sw-resize':
+        this.addSizeDirection("s-resize", yPos);
+        this.addSizeDirection("w-resize", xPos);
+        break;
+      case 'se-resize':
+        this.addSizeDirection("s-resize", yPos);
+        this.addSizeDirection("e-resize", xPos);
+        break;
+      case 'w-resize':
+        this.addSizeDirection("w-resize", xPos);
+        break;
+      case 'e-resize':
+        this.addSizeDirection("e-resize", xPos);
+        break;
+      case 'n-resize':
+        this.addSizeDirection("n-resize", yPos);
+        break;
+      case 's-resize':
+        this.addSizeDirection("s-resize", yPos);
+        break;
+    }
+  }
+
+  this.addSizeDirection = function(direction, value) {
+    switch(direction) {
+      case 'w-resize':
+        if (!this.canSize_W) break;
+        if (this.xPos + value > this.xPos + this.width) break;
+        this.width -= value;
+        this.xPos += value;
+        break;
+      case 'e-resize':
+        if (!this.canSize_E) break;
+        this.width += value;
+        break;
+      case 'n-resize':
+        if (!this.canSize_N) break;
+        if (this.yPos + value > this.yPos + this.height) break;
+        this.height -= value;
+        this.yPos += value;
+        break;
+      case 's-resize':
+        if (!this.canSize_S) break;
+        this.height += value;
+        break;
+    }
+  }
+
+  this.setCursor = function(offsetX, offsetY, width, height) {
+    const tol = 4;
+
+    if      (offsetX < 0+tol      && offsetY < 0+tol)       this.cursor = 'nw-resize';
+    else if (offsetX > width-tol  && offsetY < 0+tol)       this.cursor = 'ne-resize';
+    else if (offsetX < 0+tol      && offsetY > height-tol)  this.cursor = 'sw-resize';
+    else if (offsetX > width-tol  && offsetY > height-tol)  this.cursor = 'se-resize';
+    else if (offsetX < 0+tol)                               this.cursor = 'w-resize';
+    else if (offsetX > width-tol)                           this.cursor = 'e-resize';
+    else if (offsetY < 0+tol)                               this.cursor = 'n-resize';
+    else if (offsetY > height-tol)                          this.cursor = 's-resize';
+    else if (offsetX >= 0+tol && 
+             offsetX <= width-tol && 
+             offsetY >= 0+tol && 
+             offsetY <= height-tol)                         this.cursor = 'move';
+  }
+
+}
+
+const l = new Library("canvas");
 l.createModule();
 l.createModule();
