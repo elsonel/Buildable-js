eb0fdf73646a76024d0bf50a9845755d82691af8
Elson <elsonliang@gmail.com>
2021-11-21 00:47:11 -0500

    Refactor code

--- library.js
+++ library.js
@@ -12,39 +12,50 @@ const Library = function (id) {
   canvasElement.addEventListener("mouseup", onRelease);
   canvasElement.addEventListener('mouseleave', onRelease);
 
-  let allModules = [];
+  const allModules = {};
+  let outerModules = [];
   let activeModule = null;
+  let hoverModule = null;
+  let hoverModules = [];
 
   let isMouseDown = false;
   let startX = 0;
   let startY = 0;
 
   /* Event Callbacks */
-
   function onPress (e) {
     isMouseDown = true;
     startX = e.clientX;
     startY = e.clientY;
 
-    const clickedModule = getModuleByID(getOuterModule(e.target).id);
-    if (clickedModule) {
-      setActiveModule(clickedModule);
-      clickedModule.setCursor(e.offsetX, e.offsetY, e.target.clientWidth, e.target.clientHeight)
-    }
+    setActiveModule(hoverModule);
 
-    render();
+    render(outerModules);
   }
 
   function onMove (e) {
-    if (!isMouseDown && !activeModule) {
-      const module = getModuleByID(getOuterModule(e.target).id);
-      if (module) module.setCursor( e.offsetX, 
-                                    e.offsetY, 
-                                    e.target.clientWidth, 
-                                    e.target.clientHeight)
-    }
+    hoverModules = getHoveredModules(e.clientX, e.clientY);
+    console.log(hoverModules.map(e => e.cursor));
 
-    if (isMouseDown && activeModule) {
+    if (!activeModule) {
+      hoverModules.forEach(module => {
+        // https://htmldom.dev/calculate-the-mouse-position-relative-to-an-element/
+        // https://javascript.info/bubbling-and-capturing
+        // module.setCursor(e.offsetX, e.offsetY, e.target.clientWidth, e.target.clientHeight);
+  
+        const bounds = module._attachedHTML.getBoundingClientRect();
+        const x = e.clientX - bounds.left;
+        const y = e.clientY - bounds.top;
+        const width = bounds.width;
+        const height = bounds.height;
+  
+        module.setCursor(x, y, width, height);
+      });
+
+      if (hoverModules.at(-1)) hoverModule = hoverModules.at(-1);
+      else hoverModule = null;
+  
+    } else {
       const deltaX = (e.clientX - startX);
       const deltaY = (e.clientY - startY);
       startX = e.clientX;
@@ -54,7 +65,7 @@ const Library = function (id) {
       else activeModule.addSize(deltaX, deltaY);
     }
 
-    render();
+    render(outerModules);
   }
 
   function onRelease(e) {
@@ -71,83 +82,96 @@ const Library = function (id) {
   }
 
   function getModuleByID(id) {
-    /*https://javascript.info/bubbling-and-capturing*/
-    return allModules.find((module) => module.id == id);
+    return allModules[id];
+  }
+
+  function getHoveredModules(x, y) {
+    const hoverElements = document.elementsFromPoint(x, y);   
+    const hoverModuleElements = hoverElements.filter(e => getModuleByID(e.id));
+
+    return hoverModuleElements.map(e => getModuleByID(e.id));
   }
 
   function setActiveModule(module) {
     activeModule = module;
     
     if (module != null) {
-      allModules = allModules.filter((m) => m !== module);
-      allModules.unshift(module);
+      outerModules = outerModules.filter((m) => m !== module);
+      outerModules.unshift(module);
     }
 
-    render();
+    render(outerModules);
   }
 
-  function render() {
+  function render(allModules) {    
     [...allModules].reverse().forEach(module => {
-
-      let moduleElement = null;
-      const array = canvasElement.children;
-      for(let i = 0; i < array.length; i++)
-        if (array[i].id === module.id) {
-          moduleElement = array[i];
-          break;
-        }
-
-      if (!moduleElement) {
-        moduleElement = document.createElement('div');
-        moduleElement.className = "module";
-        moduleElement.id = module.id;
+      let moduleElement = module._attachedHTML;
+    
+      if (!module.parent) {
         moduleElement.style.position = 'absolute';
-        //moduleElement.style.paddingTop = '10px';
-        //moduleElement.innerHTML = module.id;
+        moduleElement.style.top = module.yPos;
+        moduleElement.style.left = module.xPos;
+        moduleElement.style.width = `${module.width}px`;
+        moduleElement.style.height = `${module.height}px`;
 
-        /* snapping test */
+        canvasElement.appendChild(moduleElement);
+      } else {
+        moduleElement.style.position = 'static';
+        moduleElement.style.width = '100%';
+        moduleElement.style.height = `${module.height * 0.5}px`;
 
-        const parent = moduleElement;
-
-        child1 = document.createElement('div');
-        child1.style.width = '100%';
-        child1.style.height = `${module.height * 0.5}px`;
-        child1.style.backgroundColor = 'red';
-
-        parent.appendChild(child1);
+        module.parent._attachedHTML.appendChild(moduleElement);
       }
-
-      moduleElement.style.cursor = module.cursor;
-      //canvasElement.style.cursor = activeModule ? module.cursor : "default";
-
-      moduleElement.style.top = module.yPos;
-      moduleElement.style.left = module.xPos;
-      moduleElement.style.width = `${module.width}px`;
-      moduleElement.style.height = `${module.height}px`
-
-      canvasElement.appendChild(moduleElement)
+      
+      render(module.children);
     });
+
+    canvasElement.style.cursor = hoverModule ? hoverModule.cursor : "default";
+  }
+
+
+  this.setChildToParent = function (childModule, parentModule) {
+    childModule.canMoveX = false;
+    childModule.canMoveY = false;    
+    childModule.canSize_N = false;
+    childModule.canSize_S = true;
+    childModule.canSize_W = false;
+    childModule.canSize_E = false;
+
+    childModule.parent = parentModule;
+    parentModule.children.push(childModule);
+
+    const index = outerModules.indexOf(childModule);
+    if (index > -1) outerModules.splice(index, 1);
   }
 
    /* Public Functions */
 
-  this.createModule = function () {    
-    allModules.push(new Module()); 
+  this.createModule = function () {   
+    const module = new Module();
+    
+    allModules[module.id] = module;
+    outerModules.push(module); 
 
-    render();
+    render(outerModules);
+
+    return module;
   }
 
 }
 
 const Module = function() {
-
   this.id = uuidv4();
-  this.cursor = null;
+  this.cursor = 'default';
 
+  /* parent information */
   this.xPos = 400;
   this.yPos = 400;
   this.width = 200;
   this.height = 200;
+
+  this.minWidth = 50;
+  this.minHeight = 50;
 
   this.canMoveX = true;
   this.canMoveY = true;
@@ -156,6 +180,21 @@ const Module = function() {
   this.canSize_S = true;
   this.canSize_W = true;
   this.canSize_E = true;
+
+  this._attachedHTML = (() => {
+    const moduleElement = document.createElement('div');
+    moduleElement.className = "module";
+    moduleElement.id = this.id;
+   
+    moduleElement.style.backgroundColor = Math.floor(Math.random()*16777215).toString(16);
+    
+    //moduleElement.innerHTML = this.id;
+
+    return moduleElement;
+  })();
+
+  this.parent = null;
+  this.children = [];
 
   this.addPos = function(xPos, yPos) {
     if (this.canMoveX) this.xPos += xPos;
@@ -200,21 +239,25 @@ const Module = function() {
       case 'w-resize':
         if (!this.canSize_W) break;
         if (this.xPos + value > this.xPos + this.width) break;
+        if (this.width - value < this.minWidth) value = this.minWidth - this.width;
         this.width -= value;
         this.xPos += value;
         break;
       case 'e-resize':
         if (!this.canSize_E) break;
+        if (this.width + value < this.minWidth) value = this.minWidth - this.width;
         this.width += value;
         break;
       case 'n-resize':
         if (!this.canSize_N) break;
         if (this.yPos + value > this.yPos + this.height) break;
+        if (this.height - value < this.minHeight) value = this.minHeight - this.height
         this.height -= value;
         this.yPos += value;
         break;
       case 's-resize':
         if (!this.canSize_S) break;
+        if (this.height + value < this.minHeight) value = this.minHeight - this.height;
         this.height += value;
         break;
     }
@@ -223,22 +266,24 @@ const Module = function() {
   this.setCursor = function(offsetX, offsetY, width, height) {
     const tol = 4;
 
-    if      (offsetX < 0+tol      && offsetY < 0+tol)       this.cursor = 'nw-resize';
-    else if (offsetX > width-tol  && offsetY < 0+tol)       this.cursor = 'ne-resize';
-    else if (offsetX < 0+tol      && offsetY > height-tol)  this.cursor = 'sw-resize';
-    else if (offsetX > width-tol  && offsetY > height-tol)  this.cursor = 'se-resize';
-    else if (offsetX < 0+tol)                               this.cursor = 'w-resize';
-    else if (offsetX > width-tol)                           this.cursor = 'e-resize';
-    else if (offsetY < 0+tol)                               this.cursor = 'n-resize';
-    else if (offsetY > height-tol)                          this.cursor = 's-resize';
-    else if (offsetX >= 0+tol && 
+    if      (Math.abs(offsetX) < tol        && Math.abs(offsetY) < tol)         this.cursor = this.canSize_N && this.canSize_W ? 'nw-resize' : 'default';
+    else if (Math.abs(offsetX-width) < tol  && Math.abs(offsetY) < tol)         this.cursor = this.canSize_N && this.canSize_E ? 'ne-resize' : 'default'; 
+    else if (Math.abs(offsetX) < tol        && Math.abs(offsetY-height) < tol)  this.cursor = this.canSize_S && this.canSize_W ? 'sw-resize' : 'default'; 
+    else if (Math.abs(offsetX-width) < tol  && Math.abs(offsetY-height) < tol)  this.cursor = this.canSize_S && this.canSize_E ? 'se-resize' : 'default';
+    else if (Math.abs(offsetX) < tol)                                           this.cursor = this.canSize_W ? 'w-resize' : 'default';
+    else if (Math.abs(offsetX-width) < tol)                                     this.cursor = this.canSize_E ? 'e-resize' : 'default';
+    else if (Math.abs(offsetY) < tol)                                           this.cursor = this.canSize_N ? 'n-resize' : 'default';
+    else if (Math.abs(offsetY-height) < tol)                                    this.cursor = this.canSize_S ? 's-resize' : 'default';
+    else if (offsetX >= tol && 
              offsetX <= width-tol && 
-             offsetY >= 0+tol && 
-             offsetY <= height-tol)                         this.cursor = 'move';
+             offsetY >= tol && 
+             offsetY <= height-tol)                                             this.cursor = this.canMoveX || this.canMoveY ? 'move' : 'default';
+    else                                                                        this.cursor = 'default';
   }
 
 }
 
-const l = new Library("canvas");
-l.createModule();
-l.createModule();
+const b = new Library("canvas");
+const mod1 = b.createModule();
+const mod2 = b.createModule();
+b.setChildToParent(mod1, mod2);
